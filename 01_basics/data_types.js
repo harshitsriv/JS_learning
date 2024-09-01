9dc5e81db787074527e7bbc1525893c768e28407
harshitsriv <harshit.sriv.1506@gmail.com>
2024-09-01 11:25:19 +0000

    data types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+/*
+alert("Hello") works in browser not in nodejs
+alert (3+3)
+*/
+
+console.log(3+3)
+console.log("Harshit")
+
+let name = "Harshit"
+let age = 29
+let isLoggedIn = false
+
+//number range => 2 to power53
+//bigint
+//string
+//boolean
+//null --> dataype as well as stand alone value
+//undefined --> abhi value assign nhi hui h --> different than null 
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
+
